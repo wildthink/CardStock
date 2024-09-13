61595e43027ee0015246617c1280ecb2592a75e0
Jason Jobe <box2019@jasonjobe.com>
2024-09-12 21:00:38 -0400

    wip

--- Sources/CardStock/Utilities/ValueContainer.swift
+++ Sources/CardStock/Utilities/ValueContainer.swift
@@ -13,13 +13,36 @@ struct ValueContainer <Subject> {
     var store: [AnyHashable: Any]
     var defaultValue: Subject
     
+    var styles: [any FormatStyle] = []
+    
     subscript<M>(dynamicMember key: KeyPath<Subject,M>) -> M {
         get { store[key] as? M ?? defaultValue[keyPath: key] }
     }
-
+    
     subscript<M>(dynamicMember key: WritableKeyPath<Subject,M>) -> M {
         get { store[key] as? M ?? defaultValue[keyPath: key] }
         set { store[key] = newValue }
+    }
+    
+    func demo() {
+        let f = styles[0]
+        let it = try? f.format(any: 45)
+        print(it)
+    }
+}
+
+extension FormatStyle {
+    var inputType: Any.Type  { Self.FormatInput.self }
+    var outputType: Any.Type { Self.FormatOutput.self }
+    
+    func canFormat<A>(any: Any, as out: A.Type) -> Bool {
+        return (any is FormatInput) && (A.Type.self is FormatOutput.Type)
+    }
+    
+    func format(any: Any) throws -> FormatOutput {
+        guard let ok = any as? FormatInput
+        else { throw NSError() }
+        return format(ok)
     }
 }
 
